3a03db76b390aec593e02a51692bfb8d70aabb21
lukaspodobnik <lukas.podobnik@gmail.com>
2025-08-24 20:38:58 +0200

    Add basic REPL loop.

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		var text string
+		if scanner.Scan() {
+			text = scanner.Text()
+		} else {
+			if scanner.Err() != nil {
+				fmt.Printf("Could not capture input. Got error: %v\n", scanner.Err())
+			} else {
+				fmt.Printf("Close the Pokedex due to reading EOF.\n")
+			}
+			break
+		}
+
+		input := cleanInput(text)
+		fmt.Printf("Your command was: %s\n", input[0])
+	}
 }
